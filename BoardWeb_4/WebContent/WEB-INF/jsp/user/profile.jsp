8f33f59391446012af80da543a110df8ce69434e
Administrator <db1magic@naver.com>
2020-08-31 17:38:25 +0900

    프로필이미지(업로드)

--- BoardWeb_4/WebContent/WEB-INF/jsp/user/profile.jsp
+++ BoardWeb_4/WebContent/WEB-INF/jsp/user/profile.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,16 +8,31 @@
 <title>프로필</title>
 </head>
 <body>
-	<h1>프로필</h1>
-	<div>
-		<div>
-			<img src="${data.profile_img == null ? '/img/cat.jpg' : ''}">
-		</div>
-	
-		<div>ID : ${data.user_id }</div>
-		<div>이름 : ${data.nm }</div>
-		<div>이메일 : ${data.email }</div>
-		<div>가입일시 : ${data.r_dt }</div>
-	</div>
+   <h1>프로필</h1>
+   <div>
+      <div>
+         <c:choose>
+            <c:when test="${data.profile_img != null}">
+               <img src="/img/user/${loginUser.i_user}/${data.profile_img}">
+            </c:when>
+            <c:otherwise>
+               <img src="/img/cat.jpg">
+            </c:otherwise>
+         </c:choose>
+      </div>
+      <div>
+         <form action="/user/profile" method="post" enctype="multipart/form-data">
+            <div>
+               <label>프로필 이미지: <input type="file" name="profile_img" accept="image/*"></label>
+               <input type="submit" value="업로드">
+            </div>
+         </form>
+      </div>
+   
+      <div>ID : ${data.user_id }</div>
+      <div>이름 : ${data.nm }</div>
+      <div>이메일 : ${data.email}</div>
+      <div>가입일시 : ${data.r_dt}</div>
+   </div>
 </body>
 </html>
--- BoardWeb_4/src/com/koreait/pjt/user/profileSer.java
+++ BoardWeb_4/src/com/koreait/pjt/user/profileSer.java
@@ -1,6 +1,9 @@
 package com.koreait.pjt.user;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.UUID;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +14,9 @@ import com.koreait.pjt.MyUtils;
 import com.koreait.pjt.ViewResolver;
 import com.koreait.pjt.db.UserDAO;
 import com.koreait.pjt.vo.UserVO;
-
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import java.io.*;
 
 @WebServlet("/user/profile")
 public class profileSer extends HttpServlet {
@@ -26,8 +31,60 @@ public class profileSer extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserVO loginUser = MyUtils.getLoginUser(request);
+		String savePath = request.getSession().getServletContext().getRealPath("img")+"/user/" + loginUser.getI_user(); //저장경로
+		System.out.println("save Path : " + savePath);
 		
-		doGet(request, response);
-	}
+		File directory = new File(savePath);
+		
+		if(!directory.exists()) {
+			directory.mkdirs();
+		}
+	
+		int maxFileSize = 10_485_760; //(자릿수 보기 편하도록) //최대 파일 사이즈 크기
+		String fileNm="";
+		String originFileNm = "";
+		String saveFileNm=null;
+		
+		try {
+			MultipartRequest mr = new MultipartRequest(request, savePath
+					, maxFileSize, "UTF-8", new DefaultFileRenamePolicy());
+		
+				Enumeration files = mr.getFileNames();
+				
+				if(files.hasMoreElements()) {
+					
+				String key = (String)files.nextElement();
+				fileNm = mr.getFilesystemName(key);
+				//originFileNm = mr.getOriginalFileName(key);
+				String ext = fileNm.substring(fileNm.lastIndexOf("."));
+				saveFileNm = UUID.randomUUID() + ext;
+				
+				
+				System.out.println("key : " + key);
+				System.out.println("fileNm : " + fileNm);
+				System.out.println("originalFileNm : " + originFileNm);
+				File oldFile = new File(savePath + "/" + fileNm);
+				File newFile = new File(savePath + "/" + saveFileNm); //자바메모리상에 파일객체를 만들어준것이다.
+				oldFile.renameTo(newFile);
+				}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		if(saveFileNm != null) {
+			//DB에 프로필 파일명 저장
+			
+			UserVO param = new UserVO();
+			param.setProfile_img(saveFileNm);
+			param.setI_user(loginUser.getI_user());
+			 UserDAO.updUser(param);
 
+			//i_user, user_id(3) of nm
+			//처리는 정수값으로 하는것이 좋다. 인트값이 속도가 더 빠르기 때문이다.
+			
+		}
+		response.sendRedirect("/user/profile");
+
+	}  
+	
 }
