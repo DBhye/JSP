17b929616910309effe8a996bffc1b5fa25a91e9
Administrator <db1magic@naver.com>
2020-09-07 17:52:58 +0900

    0907 matzip_1

---
+++ MatZip/src/com/koreait/matzip/Container.java
@@ -0,0 +1,59 @@
+package com.koreait.matzip;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/")
+public class Container extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     //xml에 설정해줫다.
+	//(무한루프 해결을 위해서)
+private HandlerMapper mapper;
+	
+	public Container() {
+		mapper = new HandlerMapper(); //기본생성자
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		proc(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		proc(request, response);
+	}
+	
+	private void proc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String temp = mapper.nav(request);
+	
+	
+
+		
+		if(temp.indexOf("/") >= 0) {
+
+			String isRedirect = temp.substring(0, temp.indexOf("/"));
+			System.out.println("isRedirect : " + isRedirect);
+			
+			if("redirect : ".equals(isRedirect)) {
+				response.sendRedirect(temp.substring(temp.indexOf("/")));
+			}
+			return;
+		}
+		
+		switch(temp) {
+		case "405" :
+			temp = "/WEB-INF/view/error.jsp";
+			break;
+		case "404":
+			temp = "/WEB-INF/view/notFound.jsp";
+			break;
+		}
+		request.getRequestDispatcher(temp).forward(request, response);
+	}
+
+}
